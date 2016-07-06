d2acb2f1199928afaf6b4dd261c5c03b26967c45
Connor O'Gara <ogara.connor@gmail.com>
2016-07-06 09:49:41 -0500

    removed unneccesary code/old comments

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-// var modOne = require('./modOne.js');
-// var modTwo = require('./modTwo.js');
 var modThree = require('./modThree.js');
-
-// var foo = modThree.randomToCurrency(1000,1001);
-// console.log(foo);
 
 modThree.balance();
 modThree.randomToCurrency(100,1000000);
